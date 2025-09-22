3065f0c1cedd9e54037f2647616153f0c2eaa563
Michael Barroco <michael@orbitalize.com>
2025-09-22 13:11:39 +0200

    Disable cache by default

--- geospatial-utils/main.py
+++ geospatial-utils/main.py
@@ -26,7 +26,7 @@ def main():
     convert_cmd = commands.add_parser("convert", help="Convert ED-269 to ED-318")
     convert_cmd.add_argument("input_url", help="URL of the ED-269 source file, example: https://data.geo.admin.ch/ch.bazl.einschraenkungen-drohnen/einschraenkungen-drohnen/einschraenkungen-drohnen_4326.json")
     convert_cmd.add_argument("output_file", help="Path to ED-318 output file")
-    convert_cmd.add_argument("-t", "--ttl", help="Time to live of the cached files after download in seconds (default: 10 min)", default="600")
+    convert_cmd.add_argument("-t", "--ttl", help="Time to live of the cached files after download in seconds (default: 0)", default="0")
 
     args = parser.parse_args()
 
