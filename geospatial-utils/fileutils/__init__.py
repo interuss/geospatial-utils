544134c3379e248ccc9d405c08a819f3c3f31e3e
Michael Barroco <michael@orbitalize.com>
2025-09-29 19:01:27 +0200

    Integrate PR comments

--- geospatial-utils/fileutils/__init__.py
+++ geospatial-utils/fileutils/__init__.py
@@ -1,13 +1,18 @@
+import datetime
 import hashlib
 import os
 import pathlib
-from typing import Tuple
-import requests
-import datetime
+from wsgiref.handlers import format_date_time
 
+import requests
 from loguru import logger
 
 CACHE_DIR = pathlib.Path(".cache/")
+"""Location used to cache downloaded files"""
+
+FILE_RETRIEVAL_TIMEOUT_S = 20
+"""Connection and read timeout when retrieving files"""
+
 
 def _safe_name(url: str) -> str:
     """Create a filesystem-safe name from the URL."""
@@ -15,33 +20,42 @@ def _safe_name(url: str) -> str:
     ext = os.path.splitext(url.split("?")[0])[1]
     return h + ext
 
-def _is_cached(url: str, cache_ttl_sec) -> Tuple[pathlib.Path, bool]:
+
+def get(url: str, cache_ttl_sec: int | None = None) -> pathlib.Path:
+    """Download and cache the file located at url.
+    It ignores cache entries older than cache_ttl_sec"""
+
     f = pathlib.Path(CACHE_DIR).joinpath(_safe_name(url))
 
-    creation_time = datetime.datetime.fromtimestamp(os.path.getctime(f))
-    now = datetime.datetime.now()
-    age = (now - creation_time).total_seconds()
+    if f.exists():
+        creation_time = datetime.datetime.fromtimestamp(
+            os.path.getctime(f), tz=datetime.UTC
+        )
 
-    cache_hit = f.exists() and age < cache_ttl_sec
-    if cache_hit:
-        ttl = int(cache_ttl_sec-age)
-        logger.debug(f"Cache hit for {url} with {f} (ttl: {ttl}s)")
-    return f, cache_hit
+        # Check for local cache hit
+        if cache_ttl_sec is not None:
+            now = datetime.datetime.now(tz=datetime.UTC)
+            age = (now - creation_time).total_seconds()
+            if age < cache_ttl_sec:
+                ttl = int(cache_ttl_sec - age)
+                logger.debug(f"Cache hit for {url} with {f} (ttl: {ttl}s)")
+                return f
 
-def _download_and_cache(url: str, destination: pathlib.Path):
-    CACHE_DIR.mkdir(exist_ok=True)
+        # Tell server when our copy was retrieved
+        http_date = format_date_time(creation_time.timestamp())
+        headers = {"If-Modified-Since": http_date}
+    else:
+        headers = {}
 
-    r = requests.get(url, timeout=20)
+    r = requests.get(url, headers=headers, timeout=FILE_RETRIEVAL_TIMEOUT_S)
     r.raise_for_status()
-    destination.write_bytes(r.content)
-    logger.debug(f"Downloaded {url} to {destination}")
 
-def get(url: str, cache_ttl_sec=0) -> pathlib.Path:
-    """Download and cache the file located at url.
-    It discards cache entries older than cache_ttl_sec"""
-
-    destination, cache_hit = _is_cached(url, cache_ttl_sec)
-    if not cache_hit:
-        _download_and_cache(url, destination)
-
-    return destination
+    if r.status_code == 200:
+        CACHE_DIR.mkdir(exist_ok=True, parents=True)
+        f.write_bytes(r.content)
+        logger.debug(f"Downloaded {url} to {f}")
+    elif r.status_code == 304:
+        pass  # Our copy is up to date with the server
+    else:
+        raise RuntimeError(f"Server status of {r.status_code} is not supported")
+    return f
--- geospatial-utils/main.py
+++ geospatial-utils/main.py
@@ -1,9 +1,8 @@
 import argparse
-import fileutils
 import os
-import pathlib
 import sys
 
+import fileutils
 from loguru import logger
 
 version = os.environ.get("GEOSPATIAL_UTILS_VERSION", "unknown")
@@ -12,21 +11,24 @@ version = os.environ.get("GEOSPATIAL_UTILS_VERSION", "unknown")
 def main():
     logger.info(f"Geospatial utils - {version}")
 
-    parser = argparse.ArgumentParser(
-        description="ED-269 to ED-318 converter"
-    )
+    parser = argparse.ArgumentParser(description="ED-269 to ED-318 converter")
 
     commands = parser.add_subparsers(
-        title="commands",
-        dest="command",
-        help="command",
-        required=True
+        title="commands", dest="command", help="command", required=True
     )
 
     convert_cmd = commands.add_parser("convert", help="Convert ED-269 to ED-318")
-    convert_cmd.add_argument("input_url", help="URL of the ED-269 source file, example: https://data.geo.admin.ch/ch.bazl.einschraenkungen-drohnen/einschraenkungen-drohnen/einschraenkungen-drohnen_4326.json")
+    convert_cmd.add_argument(
+        "input_url",
+        help="URL of the ED-269 source file, example: https://data.geo.admin.ch/ch.bazl.einschraenkungen-drohnen/einschraenkungen-drohnen/einschraenkungen-drohnen_4326.json",
+    )
     convert_cmd.add_argument("output_file", help="Path to ED-318 output file")
-    convert_cmd.add_argument("-t", "--ttl", help="Time to live of the cached files after download in seconds (default: 0)", default="0")
+    convert_cmd.add_argument(
+        "-t",
+        "--ttl",
+        help="Time to live of the cached files after download in seconds (default: 0)",
+        default="0",
+    )
 
     args = parser.parse_args()
 
